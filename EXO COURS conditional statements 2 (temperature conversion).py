8607a7b1876176e93ed6a26a0af7ec2fdef8e5a3
Hichem-Brahim_Djelloul <162839696+Hichem-BRHM@users.noreply.github.com>
2024-12-10 16:04:07 +0100

    Delete EXO COURS conditional statements 2 (temperature conversion).py

--- EXO COURS conditional statements 2 (temperature conversion).py
+++
@@ -1,9 +0,0 @@
-F = int(input("please type in a ttemperature : "))
-
-C = 5 / 9 * (F - 32 )
-
-print(f"{F} degrees Fahrenheit equal {C} degrees Celsius")
-
-if C < 0 :
-    print("Brr ! It's cold in here !")
-    
