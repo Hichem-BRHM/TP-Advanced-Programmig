aebbc9b602ae5ec1e43974d418e402741569f87e
Hichem-Brahim_Djelloul <162839696+Hichem-BRHM@users.noreply.github.com>
2024-12-10 15:45:04 +0100

    Update Exercise 9 explain princip.py

--- Exercise 9 TP Name Length Kingdom.py
+++ Exercise 9 TP Name Length Kingdom.py
@@ -1,3 +1,5 @@
+# le principe est de saisir autant de fois les noms des villes , et quand on termine de les saisir tous : on ecrit dans derniére saisis "stop" afin d'afficher a la fin la liste des villes triées selon leurs nombre des symbols (ou bien population)
+
 a = True
 cities = []  # Liste pour stocker les noms de villes
     
